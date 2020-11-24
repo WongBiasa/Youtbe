ba8e7f2cb0ae225d0059bfc12e827efa5afea488
WongBiasa <74451855+WongBiasa@users.noreply.github.com>
2020-11-24 19:48:16 +0700

    Add files via upload
    tutor ke ig https://www.instagram.com/wong_biasa0723/

---
+++ yt.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+
+echo "Hallo kaka"
+sleep 0.05
+echo "Jomblo ya??/"
+sleep 0.03
+echo "Jawab"
+read Jawab
+sleep 0.03
+echo "ngakak Dasar Jones:v,wkwkwwkww"
+echo
+echo "siapa nama mu?"
+read nama 
+sleep 0.05
+echo "Hallo $nama Jones:v nih"
+sleep 0.03
+echo "Masuk keTool"
+sleep 0.04
+cd /sdcard
+echo "sedang intall"
+wget https://pastebin.com/raw/LY5iibS5 -O yt.sh
+echo "selesai"
+echo "======================"
+echo "@@@@@Author@@@@@@@@@@@"
+echo "@@@@@@@@@@WhoMHw@@@@@@"
+echo "@@@@You@@Tube@@@@@@@@@"
+echo "======================"
+sleep 0.09echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+sleep 0.03
+echo "@                                                                              @"
+sleep 0.03
+echo "@                                                                              @"
+sleep 0.03
+echo "@    * * / /,&#&/** /                                    * * / /,%#@(/*        @"
+sleep 0.03
+echo "@      * & @ & #. . * & &                             #.., * % @ @ #..         @"
+sleep 0.05
+echo "@           . % @ *      . % @                     . # @ *        #            @"
+echo "@                                          #                                   @"
+
+echo "@                                                                              @"
+
+echo "@                                       *                                      @"
+
+echo "@        @ @,%&,@ @ @ @ @,%&,@ @ @ @ @ @&,@ @ @ @ @.%&,@ @ @ @ @.&&*@          @"
+
+echo "@          # % % ( .  * ( % % / .  * #/( & ( .  * ( # % ( .  * ( # %           @"
+
+echo "@                          # @ /      & # @ /                                  @"
+
+echo "@              /,,( ( ( ( ( /,,/ ( (.(#  (,,/ ( ( ( ( (,,( ( ( (               @"
+
+echo "@                                                                              @"
+
+echo "@                                                                              @"
+
+echo "@            .. , . . . , .. . . . .   ,  , . , . . ., . . , , , .             @"
+
+echo "@              & & .      . & & .            .      ..% & .                    @"
+
+echo "@             , @ @ @ #    , & @              & # .  , & @ @ #                 @"
+
+echo "@               %*@ @ @ & @.&                     @ @.&#/@ @                   @"
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo
+echo "================================="
+bash yt.sh
+echo "================================="
+echo "Tutor dm IG https://www.instagram.com/siapa_namasaya23/ Atau https://www.instagram.com/wong_biasa0723/"
+
